1e9c60b7a0af1afe7ab2648c7476cebcf6865127
TranTriDat <tridat123123@gmail.com>
2022-12-22 16:34:08 +0800

    add code

---
+++ android/app/src/main/kotlin/com/fruitapp/fruit_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fruitapp.fruit_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tflite/tflite.dart';
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Fruit Classification App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
